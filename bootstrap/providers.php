2bcb629e4276366d2b79f72758367b0c030372d0
Francky <kyfr.rakotonirina1@gmail.com>
2025-04-01 11:08:08 +0200

    fixed

--- bootstrap/providers.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\FortifyServiceProvider::class,
-    App\Providers\JetstreamServiceProvider::class,
-    App\Providers\SocialstreamServiceProvider::class,
-    App\Providers\SoftaculousServiceProvider::class,
-];
--- resources/js/utils/assetHelper.js
+++
@@ -1,4 +0,0 @@
-// resources/js/utils/assetHelper.js
-export function getAssetUrl(path) {
-    return new URL(`@assets/${path}`, import.meta.url).href
-}
