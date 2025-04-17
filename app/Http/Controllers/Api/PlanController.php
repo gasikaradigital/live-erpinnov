73cbdfb512f62d3914555ad1caa0061c80e578b9
Francky <kyfr.rakotonirina1@gmail.com>
2025-04-17 07:01:50 +0200

    update api/plan via api Dolibarr

--- app/Http/Controllers/Api/PlanController.php
+++ app/Http/Controllers/Api/PlanController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Plan;
 use App\Models\SubPlan;
+use Illuminate\Support\Facades\Http;
 
 class PlanController extends Controller
 {
@@ -14,16 +15,72 @@ class PlanController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
+
+      // Constantes
+    const TYPE_PRODUIT = 0;
+    const TYPE_SERVICE = 1;
+
+    const STATUS_HORS_VENTE = 0;
+    const STATUS_EN_VENTE = 1;
+
+    const STATUS_HORS_ACHAT = 0;
+    const STATUS_EN_ACHAT = 1;
+
+    public $data;
+
     public function plan()
     {
         $user = auth()->user();
+        try {
+            
+            $response = Http::withHeaders([
+                'DOLAPIKEY' => 'V8ARU7g614rfiu5Dft2fbj4P6xXDO9TN',
+            ])->get('https://g.erpinnov.com' . '/api/index.php/products', [
+                'limit' => 100,
+                'sortfield' => 'ref',
+                'sortorder' => 'ASC',
+            ]);
 
-        $plans = Plan::all();
-        $subPlans = SubPlan::all();
+            if (!$response->successful()) {
+                return response()->json([
+                    'message' => 'Failed to retrieve plans',
+                ]);
+            }
 
-        return response()->json([
-            'plans' => $plans,
-            'subPlans' => $subPlans,
-        ]);
+            $data = collect($response->json())->map(function ($item) {
+                return (object) [
+                    'id' => $item['id'],
+                    'ref' => $item['ref'],
+                    'label' => $item['label'],
+                    'description' => $item['description'] ?? '',
+                    'type' => $item['type'],
+                    'type_label' => $item['type'] == self::TYPE_PRODUIT ? 'Produit' : 'Service',
+                    'price' => number_format($item['price'], 2, ',', ' ') . ' €',
+                    'price_min' => number_format($item['price_min'], 2, ',', ' ') . ' €',
+                    'price_ttc_formatted' => number_format($item['price_ttc'], 2, ',', ' ') . ' €',
+                    'tva_tx' => $item['tva_tx'],
+                    'status' => $item['status'],
+               
+                ];
+            })->all();
+
+            
+                //Récupère seulement les produits
+            foreach($data as $produit)
+            {
+                if($produit->type == 0){
+                    $this->data[] = $produit;
+                }
+            }
+
+            return response()->json([
+                'plan' => $this->data,
+            ], 200);
+
+        } catch (Exception $e) {
+            return response()->json([
+                'erreur' => $e->getMessage(),
+            ]);
+        }
     }
 }
