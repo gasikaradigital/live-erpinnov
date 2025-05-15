fc4585a665c350c86a18846d6b6c1a19d63185b4
Francky <kyfr.rakotonirina1@gmail.com>
2025-05-15 10:10:24 +0200

    cors.php

---
+++ test-cors.php
@@ -0,0 +1,13 @@
+<?php
+header("Access-Control-Allow-Origin: https://portail.erpinnov.com");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
+echo json_encode(['status' => 'CORS OK']);
+
