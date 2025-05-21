13c9793482a5f6def5bb328eb833550757b7a03d
haritsimba <haritsimbafranck@gmail.com>
2025-05-21 10:24:54 +0300

    update : plans (database)

--- app/Http/Controllers/Api/PlanController.php
+++ app/Http/Controllers/Api/PlanController.php
@@ -10,13 +10,13 @@ use Illuminate\Support\Facades\Http;
 
 class PlanController extends Controller
 {
-        /**
+    /**
      * Returns a JSON response containing all plans and sub-plans.
      *
      * @return \Illuminate\Http\JsonResponse
      */
 
-      // Constantes
+    // Constantes
     const TYPE_PRODUIT = 0;
     const TYPE_SERVICE = 1;
 
@@ -30,12 +30,19 @@ class PlanController extends Controller
 
     public function plan()
     {
+
+        $this->data = $this->getFromDatabase();
+        return $this->data;
+    }
+
+    private function fetchFromDolibarr()
+    {
         $user = auth()->user();
         try {
-            
+
             $response = Http::withHeaders([
                 'DOLAPIKEY' => 'V8ARU7g614rfiu5Dft2fbj4P6xXDO9TN',
-            ])->get('https://g.erpinnov.com' . '/api/index.php/products', [
+            ])->get('https://gmg.erpinnov.com' . '/api/index.php/products', [
                 'limit' => 100,
                 'sortfield' => 'ref',
                 'sortorder' => 'ASC',
@@ -60,27 +67,63 @@ class PlanController extends Controller
                     'price_ttc_formatted' => number_format($item['price_ttc'], 2, ',', ' ') . ' €',
                     'tva_tx' => $item['tva_tx'],
                     'status' => $item['status'],
-               
+
                 ];
             })->all();
 
-            
-                //Récupère seulement les produits
-            foreach($data as $produit)
-            {
-                if($produit->type == 0){
+
+            //Récupère seulement les produits
+            foreach ($data as $produit) {
+                if ($produit->type == 0) {
                     $this->data[] = $produit;
                 }
             }
 
-            return response()->json([
-                'plan' => $this->data,
-            ], 200);
-
+            return $data;
         } catch (Exception $e) {
             return response()->json([
                 'erreur' => $e->getMessage(),
             ]);
         }
+    }
+
+    private function getFromDatabase()
+    {
+        $plans = Plan::all();
+        $data = [];
+        foreach ($plans as $plan) {
+             array_push($data,$this->mapFromModel($plan));
+        }
+        return $data;
+    }
+
+    private function mapFromModel(Plan $plan)
+    {
+
+        $price_min = $plan->subPlans[0]->price_local;
+
+        $sub_plans = [];
+        foreach ($plan->subPlans as $subplan) {
+
+            if ($price_min > $subplan->price_local) {
+                $price_min = $subplan->price_local;
+            }
+            array_push($sub_plans, [
+                'id'=>$subplan->id,
+                'label'=>$subplan->name,
+                'features'=>$subplan->features,
+                'price_monthly'=>$subplan->price_monthly,
+                'price_yearly'=>$subplan->price_yearly,
+                'price_monthly_formated'=>$subplan->price_local
+            ]);
+        }
+        return [
+            'id' => $plan->id,
+            'label' => $plan->name,
+            'description' => $plan->description,
+            'features' => $plan->features,
+            'price_min' => $price_min,
+            'sub_plans' => $sub_plans
+        ];
     }
 }
