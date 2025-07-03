f23ce05210d9928fe2c0b71a2f5ac667f5c9fb4f
Francky <kyfr.rakotonirina1@gmail.com>
2025-07-03 10:11:48 +0200

    step 2 debug

--- app/Http/Controllers/Api/DolisassController.php
+++ app/Http/Controllers/Api/DolisassController.php
@@ -25,9 +25,9 @@ class DolisassController extends Controller
         }
 
         try{
-            //$dolisass = new DolisassServices($request->all());
+            $dolisass = new DolisassServices($request->all());
             return response()->json([
-                'req' => 'tong',
+                'entreprise' => $dolisass,
             ], 201);
         } catch (\Exception $e) {
             return response()->json([
