e09266c68a3162d313c426692893adfc6a85b2e7
Francky <kyfr.rakotonirina1@gmail.com>
2025-07-29 08:15:48 +0200

    test password

--- app/Services/CreateUsersDolibarr.php
+++ app/Services/CreateUsersDolibarr.php
@@ -96,7 +96,7 @@ class CreateUsersDolibarr
             DB::reconnect('dynamic');
            
             DB::connection('dynamic')->table($instance_free->prefix.'user')
-                ->where('rowid', 1)
+                ->where('rowid', 9)
                 ->update([
                     'pass_crypted' => $passwordHash
                 ]);
